578a382d14627e5957a04bf353e28abb61ae6de7
6kbgdb7k85-stack <6kbgdb7k86@privaterelay.appleid.com>
2026-07-15 20:03:25 -0400

    Lab Complete

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,17 @@
 import { useEffect, useState } from "react";
 
+/**
+ *
+ * @param {string} key
+ * @param {string} initialValue
+ */
+
 export function useLocalStorage(key, initialValue = null) {
+  const [value, setValue] = useState(localStorage.getItem(key) || initialValue);
+
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value]);
+
+  return [ value, setValue ];
 }
